d219da0210a1786ca0f7f5e80dcbbd0d953f8987
Alex Cross <AlexJCross90@gmail.com>
2025-01-02 20:36:06 +0000

    Fix type hint

--- python/genius_square/experimental/__init__.py
+++ python/genius_square/experimental/__init__.py
@@ -1,13 +1,11 @@
 from ..printer import Printer
 from ._solve import Counter, GameState, Solver, hello_from_bin
-from .solver import ExtendedGameState
 
 # Hacky - sorry - time boxed project!
 GameState.__repr__ = lambda self: Printer().str_repr(self.board, self.history)
 
 __all__ = [
     "Counter",
-    "ExtendedGameState",
     "GameState",
     "hello_from_bin",
     "Solver",
